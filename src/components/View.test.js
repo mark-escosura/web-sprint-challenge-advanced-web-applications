a634439529cdf1e75b804a311619c8802187395a
mark-escosura <escosuramarkse@gmail.com>
2021-11-12 16:58:59 -0800

    Adavanced Testing: View.test.js but only Test 1 is Completed | Completed articleServices.js file

--- src/components/View.test.js
+++ src/components/View.test.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
+import articleService from './../services/articleServices'
 import View from './View';
 
-test("renders zero articles without errors", async () => {
-});
+jest.mock('./../services/articleServices');
 
-// test("renders three articles without errors", async ()=> {
-// });
+describe ('View 1st Test', () => {
+    test("renders zero articles without errors", async () => {
+        articleService.mockResolvedValueOnce([]);
+    
+        render(<View/>);
+    
+        await waitFor (() => {
+            const articles = screen.queryAllByTestId("article");
+            expect(articles).toHaveLength(0);
+        })
+    });
+})
+
+describe ('View 2nd Test', () => {
+    test("renders three articles without errors", async ()=> {
+        articleService.mockResolvedValueOnce([ /* cant do it no more time*/]);
+    });
+})
+
+
+
+test("renders three articles without errors", async ()=> {
+});
 
 //Task List
 //1. Complete the above two tests. Make sure to mocking the articleService call before rendering.
--- src/services/articleServices.js
+++ src/services/articleServices.js
@@ -1,6 +1,10 @@
-
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const articleService = ()=> {
+    axiosWithAuth().get('/articles')
+    .then(res => {
+        return (res.data);
+    })
 }
 
 export default articleService;
